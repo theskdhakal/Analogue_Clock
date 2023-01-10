629c5fe67c1768fe8166f36a76bedb25ddf965e2
Shiva Dhakal <theskdhakal@gmail.com>
2023-01-10 14:46:04 +1100

    analogue watch using Js, Html and css

---
+++ script.js
@@ -0,0 +1,26 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector("[data-hour-hand]");
+console.log(hourHand);
+const minuteHand = document.querySelector("[data-minute-hand]");
+console.log(minuteHand);
+const secondHand = document.querySelector("[data-second-hand]");
+console.log(secondHand);
+
+function setClock() {
+  const currentDate = new Date();
+
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
